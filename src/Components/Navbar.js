dc3c58ed71f3377868c33725f263dc604de14d7d
Arya Yunanta <aryayun90@gmail.com>
2022-03-20 11:17:26 +0700

    Add Home page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,20 +3,20 @@ import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="w-screen flex justify-between items-center shadow-md px-12 py-2">
+        <nav className="w-screen flex justify-between items-center shadow-md px-12 py-2 lg:text-sm xl:text-base fixed top-0 bg-white z-50" >
             <div className="flex items-center">
-                <img src="/gdsc-unsri-logo.png" alt="gdsc-unsri-logo" className="w-16 h-fit"/>
+                <img src="/gdsc-unsri-logo.png" alt="gdsc-unsri-logo" className="xl:w-16 lg:w-12 h-fit"/>
                 <div className="ml-3">
-                    <p className="font-medium text-gray-900">Google Developer Student Clubs</p>
-                    <p className="font-light text-gray-800">Universitas Sriwijaya</p>
+                    <p className="font-medium text-gray-800">Google Developer Student Clubs</p>
+                    <p className="font-light text-gray-500">Universitas Sriwijaya</p>
                 </div>
             </div>
-            <div className="space-x-12 text-gray-700 hover:text-gray-900 transition">
-                <Link to='/'>Home</Link>
-                <Link to='/recordings'>Webinar & Workshop Recordings</Link>
-                <Link to='/events'>Upcoming Events</Link>
-                <Link to='/members'>Members</Link>
-                <Link to='/about'>About GDSC</Link>
+            <div className="space-x-12 text-gray-600">
+                <Link className="hover:text-gray-900 transition" to='/'>Home</Link>
+                <Link className="hover:text-gray-900 transition" to='/recordings'>Recordings</Link>
+                <Link className="hover:text-gray-900 transition" to='/events'>Upcoming Events</Link>
+                <Link className="hover:text-gray-900 transition" to='/members'>Members</Link>
+                <Link className="hover:text-gray-900 transition" to='/about'>About GDSC</Link>
             </div>
         </nav>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {},
+        fontFamily: {
+            'sans': ['"Google Sans"'],
+        }
     },
     plugins: [],
 }

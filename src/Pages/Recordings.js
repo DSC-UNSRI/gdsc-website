11041ad0821baf46aa1b51022eaeaf9bbf325679
Irvan Malik <irvanmalik48@gmail.com>
2022-04-03 00:41:52 +0700

    Some adjustment for `Recordings` page
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/Pages/Recordings.js
+++ src/Pages/Recordings.js
@@ -7,7 +7,7 @@ export default function Community() {
             {/* WIP carousel */}
             <div className="mt-12 px-20 xl:px-28">
                 <h2 className="text-lg xl:text-xl font-bold mb-3 text-[#4285F4]">Recordings</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2 md:gap-7">
                     <EventItem 
                         title="Webinar Title"
                         img="/webinar.jpg"

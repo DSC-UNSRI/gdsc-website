6b8586cad625170668b66f66c76012a4cb14a6dd
zidane-arkan <zidark897@Gmail.com>
2022-09-06 18:50:35 +0700

    Add Html Structure

--- src/Pages/RecordingVideo.js
+++ src/Pages/RecordingVideo.js
@@ -4,9 +4,38 @@ function RecordingVideo() {
     return (
         <main className="w-screen relative leading-relaxed text-gray-900 md:bg-amber-300 lg:bg-white overflow-x-hidden mt-16 pb-28">
             <article>
-                
+                <section></section>
+                <section></section>
             </article>
-            <article></article>
+            <article>
+                <section>
+                    <div></div>
+                    <div>
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h3></h3>
+                        <div>
+                            <div>
+                                <h4></h4>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h3></h3>
+                        <div>
+                            <div>
+                                <h4></h4>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </article>
             <article></article>
         </main>
     );

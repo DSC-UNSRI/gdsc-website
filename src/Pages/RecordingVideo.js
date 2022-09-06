e02a9d66b333a08fc19fbff5a23c42155094758f
zidane-arkan <zidark897@Gmail.com>
2022-09-06 22:30:45 +0700

    Update Recording Video Component

--- src/Pages/RecordingVideo.js
+++ src/Pages/RecordingVideo.js
@@ -2,22 +2,28 @@ import React from 'react';
 
 function RecordingVideo() {
     return (
-        <main className="w-screen relative leading-relaxed text-gray-900 md:bg-amber-300 lg:bg-white overflow-x-hidden mt-16 pb-28">
+        <main className="w-screen relative leading-relaxed text-gray-900 md:bg-amber-300 lg:bg-white overflow-x-hidden mt-32 pb-28 px-20 xl:px-28">
             <article>
                 <section></section>
                 <section></section>
             </article>
-            <article>
-                <section>
-                    <div></div>
-                    <div>
-                        <h3></h3>
-                        <p></p>
+            <article className="w-screen relative flex 2xl:flex-col items-start ">
+                <section className="w-screen flex flex-col 2xl:flex-col gap-6 items-start">
+                    <div className="flex flex-col gap-2 text-black">
+                        <h1 className="font-semibold text-lg xl:text-4xl 2xl:text-4xl">Cracking Coding Interview</h1>
+                        <span className="text-[#A3A3A3] font-medium text-lg 2xl:text-2xl">March 6, 2022</span>
+                    </div>
+                    <div className="w-[70vw] flex flex-col gap-3">
+                        <h3 className="font-semibold text-xl xl:text-2xl 2xl:text-3xl">Description</h3>
+                        <div className="w-[70vw] flex flex-col">
+                            <span className="text-[#A3A3A3] font-normal text-lg 2xl:text-2xl">Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor.</span>
+                            <span className="text-[#A3A3A3] font-normal text-lg 2xl:text-2xl">lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum.</span>
+                        </div>
                     </div>
                 </section>
-                <section>
+                <section className="w-screen flex 2xl:flex-col gap-16">
                     <div>
-                        <h3></h3>
+                        <h3>Speakers</h3>
                         <div>
                             <div>
                                 <h4></h4>
@@ -26,7 +32,7 @@ function RecordingVideo() {
                         </div>
                     </div>
                     <div>
-                        <h3></h3>
+                        <h3>Facilitator</h3>
                         <div>
                             <div>
                                 <h4></h4>
